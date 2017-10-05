ee59ed7a5a4aa4ae395a4a3dac8effcd344d998c
3x10ce <hh3_24@yahoo.co.jp>
2017-10-05 21:02:22 +0900

    Add friends/ids and friendships/lookup wrapper
    close #7

--- src/TwitterClient.js
+++ src/TwitterClient.js
@@ -38,6 +38,30 @@ const TwitterClient = class {
   remove (id) {
     return this.twitter.post('friendships/destroy', {id: id})
   }
+
+  /** 
+   * フォローしているユーザのIDを取得する
+   * [hint] すべて取得しきれなかった場合、responseのnext_cursor_str,
+   * previous_cursor_strを使って再リクエストすれば続きを取得できる。
+   * @param {number} count (optional) 取得するユーザIDの数。未指定の場合は100。
+   * @param {string} cursor (optional) ページング取得用のカーソル
+   * @return {Promise} 結果をresolve/rejectするPromiseオブジェクト
+   */
+  getIds (count, cursor) {
+    if (!count) count = 100
+    let request_parameter = { count: count }
+    if (cursor) request_parameter.cursor = cursor
+
+    return this.twitter.get('friends/ids', request_parameter)
+  }
+  /**
+   * 配列で渡されたユーザIDのフォロー状態を確認する
+   * @param {[string]} ids ユーザIDの配列
+   * @return {Promise} 結果をresolve/rejectするPromiseオブジェクト
+   */
+  getFriendships (ids) {
+    return this.twitter.get('friendships/lookup', {user_id: ids.join(',')})
+  }
 }
 
 module.exports = TwitterClient
