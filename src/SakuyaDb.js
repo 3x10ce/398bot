e23ba3f93151cf911be673ba0035961fbae6616b
3x10ce <hh3_24@yahoo.co.jp>
2017-10-12 18:16:54 +0900

    Add mock of user data structure

--- src/SakuyaDb.js
+++ src/SakuyaDb.js
@@ -17,18 +17,39 @@ let SakuyaDb = class {
   createUser (user) {
     let collection = this.db.collection('users')
     collection.insertOne({
-      'id': user.id_str,
-      'name': user.name
+      '_id': user.id_str,
+      'name': user.name,
+      'nickname': ':NAME:さん',
+      'lovelity': 5
     })
   }
 
-  updateUser (user, data) {
+  _updateUser (id, data) {
     let collection = this.db.collection('users')
     collection.findOneAndUpdate({
-      'id': user.id_str      
+      '_id': id
     }, data)
+  }
 
+  setNickname (id, nickname) {
+    return this._updateUser(id,
+      {$set:{ 'nickname': nickname}}
+    )
   }
 }
 
 module.exports = SakuyaDb
+
+let MongoDb = require('mongodb').Db
+let MongoServer = require('mongodb').Server
+
+let server = new MongoServer('localhost', 27017)
+let db = new MongoDb('SakuyaBot', server, {safe: true})
+
+let sdb = new SakuyaDb(db)
+
+setTimeout( () => {
+  // sdb.createUser({id_str: '0000000', name: 'test'})
+  // sdb.modifyUser('0000000', {id: '0000000', name: 'test2'})  
+  sdb.setNickname('0000000', ':NAME_ORG:さん')
+},1000)
