77a714b6560723a7beb826a0dfa98f6151c02edd
3x10ce <hiro.h.324@gmail.com>
2017-11-15 12:56:55 +0900

    リプライパターン定義の分離

--- src/SakuyaReplies.js
+++ src/SakuyaReplies.js
@@ -1,22 +1,28 @@
 'use strict'
-
+const reactions = require('./res/reactions.json')
 
 /**
  * リプライパターン定義
  * あとあと、外部ファイルから読み込ませるようにするが、
  * reactionsデータのリファレンスとして、以下はコメントアウトで残しておく。
  */
+/*
 const reactions = {
-  "お(かえ|帰)り": {
-    reply_patterns: [
-      "ただいま。心配かけたわね。"
+  "(?:メリー)?クリスマス": {   // リアクションのトリガーワード(正規表現)をキーに設定する。
+    reply_patterns: [       // 返答するメッセージの配列。Botでこのパターンからランダムに1つ選択する。
+      "リプライ1",
+      "リプライ2",
+      "リプライ3"
     ],
-    requires: {
-      min_lovelity: 10
+    requires: {             // 反応する際の条件をここに記載する。
+      date_m: 12,           // 特定の日付限定のリアクションはdate_mとdate_dにその日付を設定する。
+      date_d: 25,
+      min_lovelity: 20      // 必要な好感度の値(最小値)を設定する。
     },
-    lovelity: 1
-  }, 
+    lovelity: 2             // 反応した際に上昇する好感度の数値を設定する。
+  }
 }
+ */
 
 /**
  * ツイートを元にリプライを生成する
@@ -32,14 +38,12 @@ module.exports = function(tweet, user) {
     // マッチパターンに一致するかチェック 一致しなければ終了
     let matcher = new RegExp(pattern)
     if (!matcher.test(tweet.text)) continue
-
     // 条件判定
     // requiresパラメータがある場合は、そこに記載される
     // 条件をすべてpassしているかチェックする。
     if (reaction.requires) {
 
       // すべての条件についてreduceをつかってANDをとっていく
-      console.log(reaction.requires)
       let passed = Object.keys(reaction.requires).reduce((reduced, type) => {
         let value = reaction.requires[type]
         let evaluated = false
@@ -53,8 +57,8 @@ module.exports = function(tweet, user) {
         case 'max_lovelity':
           evaluated = (user.lovelity <= value); break
         }
-
         console.log(`${type}: ${value} -> ${evaluated}` )
+
         return reduced && evaluated
       }, true)
 
