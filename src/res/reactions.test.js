eabd0373d8aac733f593a6de4f0ce7d49323f225
3x10ce <hiro.h.324@gmail.com>
2017-11-16 19:03:54 +0900

    reactions JSONのフォーマットチェッカ実装

---
+++ src/res/reactions.test.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+
+/**
+ * reactions.test.js
+ * reactions.jsonの書式をチェックします。
+ */
+'use strict'
+
+const reactions = require('./reactions.json')
+const assert = require('chai').assert
+
+describe('check reactions.json', () => {
+  it('トップレベルドメインのフォーマット', () => {
+    assert.strictEqual(typeof reactions, 'object') 
+  })
+})
+
+describe('check each reaction format', () => {
+  Object.keys(reactions).forEach( (pattern) => {
+    let reaction = reactions[pattern]
+    describe(pattern, () => {
+      it('マッチパターンの正規表現チェック', () => {
+        assert.strictEqual((new RegExp(pattern)) instanceof RegExp, true)
+      })
+
+      it('リプライパターンがStringの配列', () => {
+        assert.strictEqual(reaction.reply_patterns instanceof Array, true)
+        for (let p of reaction.reply_patterns)
+          assert.strictEqual(typeof p, 'string')
+      })
+
+      it('好感度増減値が整数値', () => {
+        assert.strictEqual(typeof reaction.lovelity, 'number')
+        assert.strictEqual(Number.isInteger(reaction.lovelity), true)
+      })
+
+      it ('必要条件がある場合は各条件もチェックする', () => {
+        if (reaction.requires) {
+          assert.strictEqual(typeof reaction.requires, 'object')
+
+          // 使用可能なtype値
+          let available_types = [
+            {id: 'date_m', type: 'number'},
+            {id: 'date_d', type: 'number'},
+            {id: 'min_lovelity', type: 'number'}
+          ]
+          Object.keys(reaction.requires).forEach( (t) => {
+            let type_scheme_index = available_types.findIndex( (e) => e.id === t)
+            assert.notStrictEqual(type_scheme_index, -1)
+
+            let type_scheme = available_types[type_scheme_index]
+            assert.strictEqual(typeof reaction.requires[t], type_scheme.type, t)
+          })
+        }
+      })
+    })
+  })
+})
