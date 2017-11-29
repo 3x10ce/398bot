12c960d65970cc616ea003205199530a7dafe6ba
3x10ce <hiro.h.324@gmail.com>
2017-11-29 18:31:25 +0900

    DataManagerのスケルトンコード追加

---
+++ src/DataManager.js
@@ -0,0 +1,124 @@
+'use strict'
+
+/**
+ * データストアとの中間層として機能するデータ管理クラスです。
+ * SakuyaBotが保持するユーザデータの管理を行い、バックエンドでデータストアとの同期を実行します。
+ */
+const DataManager = class {
+  /**
+   * 生成時、バックエンドのデータストアからデータを取得します。
+   */
+  constructor (db) {
+    /** @todo データストアからデータを取得する */
+    this.db = db
+  }
+
+  /**
+   * ユーザのドキュメントを作成します。
+   * @param {UserObject} user TwitterのUser Object
+   * @return insertの結果
+   */
+  createUser (user) {
+    /** @todo 関数の実装 */
+    return user
+  }
+
+  /** 
+   * ユーザデータを取得します。
+   * @param {UserObject} user TwitterのUser Object
+   * @return ユーザデータ
+   */
+  getUser (user) {
+    /** @todo 関数の実装 */
+    return user
+  }
+
+  /**
+   * ユーザのドキュメントを更新します。
+   * @param {UserObject} user TwitterのUser Object
+   * @param {object} data 更新するデータのクエリ
+   * @return updateの結果
+   */
+  _updateUser (user, data) {
+    /** @todo 関数の実装 */
+    return [user, data]
+  }
+
+  /**
+   * 指定したIDのユーザにニックネームを設定します。
+   * @param {UserObject} user TwitterのUser Object
+   * @param {string} nickname ニックネーム
+   * @return updateの結果
+   */
+  setNickname (user, nickname) {
+    /** @todo 関数の実装 */
+    return [user, nickname]
+  }
+
+
+  /**
+   * 指定したIDのユーザの誕生日を設定します。
+   * @param {UserObject} user TwitterのUser Object
+   * @param {number} month 誕生月
+   * @param {number} day 誕生日
+   * @return updateの結果
+   * [hint]月と日のみで管理する。Date.yearは2000でMASKする。
+   */
+  setBirthday (user, month, day) {
+    /** @todo 関数の実装 */
+    return [user, month, day]
+  }
+
+  /**
+   * 指定したIDのユーザの好感度を増減します。
+   * @param {UserObject} user TwitterのUser Object
+   * @param {number} incremental 増減量
+   * @return updateの結果
+   */
+  increaseLovelity (user, incremental) {
+    /** @todo 関数の実装 */
+    return [user, incremental]
+  }
+
+  /**
+   * 指定したIDのユーザの献血履歴を残します。
+   * @param {UserObject} user 
+   * @param {number} 献血量
+   * @return updateの結果
+   */
+  addDonateLog(user, amount) {
+    /** @todo 関数の実装 */
+    return [user, amount]
+  }
+
+  /**
+   * 指定したユーザが献血していたかどうかを調べます。
+   * @param {UserObject} user
+   * @return 献血している場合はtrue, それ以外はfalse
+   */
+  userIsDonated(user) {
+    /** @todo 関数の実装 */
+    return user
+  }
+
+  /**
+   * 献血履歴テーブルから合計量を集計します。
+   * @return {number} 献血量
+   */
+  sumDonation () {
+    /** @todo 関数の実装 */
+    return false
+  }
+
+  /**
+   * 合計献血量を日別献血履歴に退避し、
+   * 献血履歴テーブルをクリアします。
+   * @return delete結果
+   */
+  stashDonatedLog () {
+    /** @todo 関数の実装 */
+    return false
+  }
+}
+
+module.exports = DataManager
