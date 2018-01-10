2bf2a5e23476b40fdc06c04f9af10b35147143fb
3x10ce <hiro.h.324@gmail.com>
2018-01-10 21:09:18 +0900

    user tableのmockを追加

--- test/mock/Database.mock.js
+++ test/mock/Database.mock.js
@@ -1,9 +1,19 @@
 let twitter = require('./Twitter.mock')
 
+let createUserDBMock = (id) => {
+  return {
+    id: id,
+    nickname: ':NAME:さん',
+    birth_m: 1,
+    birth_d: 1,
+    lovelity: 20
+  }
+}
+
 // dbのmock/stub
 let db = {
   createUser: (user) => user,
-  getUser: (id) => Promise.resolve(twitter.userMock(id)),
+  getUser: (id) => Promise.resolve(createUserDBMock(id)),
   setNickname: (id, nickname) => Promise.resolve([id, nickname]),
   setBirthday: (id, m, d) => Promise.resolve([id, m, d]),
   increaseLovelity: (id, lovelity) => Promise.resolve([id, lovelity]),
